3ed505fd3bd56522a76250ddd1f9651c7520d54c
YesicaGonzalezN <99261724+YesicaGonzalezN@users.noreply.github.com>
2022-03-07 16:09:46 -0300

    Incorporacion array js

--- archivo.js
+++ archivo.js
@@ -1,34 +1,43 @@
-let calcular = alert("¡Bienvenido a AquaLife! A continuación calcularemos la cantidad de agua que debes beber por día en función de tu peso y altura.")
-let pesoIngresado = prompt("¿Cual es tu peso actual?")
-let alturaIngresada = prompt("¿Cual es tu altura actual?")
+const saludar = nombre => {
+    let bienvenida = `¡Hola ${nombre}, bienvenido/a a AquaLife! A continuación calcularemos la cantidad de agua que debes tomar en función de tu peso y estatura."`
+    alert(bienvenida);
+}
 
-function calcularAgua() {
+saludar(prompt("¿Cual es tu nombre?"))
+
+
+let pesoIngresado = prompt("¿Cual es tu peso actual?");
+let alturaIngresada = prompt("¿Cual es tu altura actual?");
+
+const calcularAgua = () => {
     if (pesoIngresado <= 65 && alturaIngresada <= 1.75) {
-        alert ("Tenes que tomar 2lts de agua diarios.")
+        alert("Tenes que tomar 2lts de agua diarios.")
     } else if (pesoIngresado >= 65 && alturaIngresada >= 1.60) {
-        alert ("Tenes que tomar 3lts de agua diarios.")
+        alert("Tenes que tomar 3lts de agua diarios.")
     } else {
-        alert ("Los datos ingresados son inválidos.")
+        alert("Los datos ingresados son inválidos.")
     }
 }
 
 calcularAgua()
 
 
+
 class Usuario {
-    constructor(name,informacion) {
-       this.name = name
-       this.informacion = informacion
-    } 
+    constructor(nombre, apellido, edad, genero) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.genero = genero;
+    }
 }
-
-
-let datos = ['Nombre', 'Apellido', 'Edad', 'Genero']
-let usuarios = []
-
-for (const dato of datos) {
-    let informacion= prompt(`¿Cual es tu ${dato}?`)
-    usuarios.push(new Usuario(dato, informacion))
-}
-
+    
+let usuarios = [];
+    
+let nombre = prompt(`¿Cual es tu Nombre?`);
+let apellido = prompt("¿Cual es tu Apellido?");
+let edad = prompt("¿Cual es tu Edad?");
+let genero = prompt("¿Cual es tu Género?");
+usuarios.push(new Usuario(nombre, apellido, edad, genero));
+    
 console.log(usuarios);
