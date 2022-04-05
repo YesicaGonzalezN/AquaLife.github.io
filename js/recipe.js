4feccd5680ca486a95e521896292051f8aa01181
YesicaGonzalezN <99261724+YesicaGonzalezN@users.noreply.github.com>
2022-04-05 16:46:18 -0300

    agregado de api

--- js/recipe.js
+++ js/recipe.js
@@ -57,10 +57,54 @@ searchButton.addEventListener("click", ()=> {
 async function sendApiRequest() {
     let APP_ID = "cd93eced";
     let API_KEY = "c3f9bf4235b020458764661a9a26e34e"
+    let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=taco`)
+    console.log(response)
+    let data = await response.json()
+    console.log(data)
+    useApiData(data)
+} 
+
+function useApiData(data) {
+    document.querySelector("#content").innerHTML = `
+    <div class="card-search">
+    <img src="${data.hits[0].recipe.image}" class="img-card-search" alt="..."/>
+    <div class="card-body">
+        <h5 class="card-title-search">${data.hits[0].recipe.label}</h5>
+        <p class="card-text-search">${data.hits[0].recipe.source}</p>
+        <a href="${data.hits[0].recipe.url}" class="btn btn-primary">Ver mas</a>
+    </div>
+    </div>
+    `
+}
+
+let searchButtonC = document.querySelector("#button-recipe1");
+
+searchButtonC.addEventListener("click", ()=> {
+    console.log("Boton presionado")
+    sendApiRequestC()
+})
+
+async function sendApiRequestC() {
+    let APP_ID = "cd93eced";
+    let API_KEY = "c3f9bf4235b020458764661a9a26e34e"
     let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=pizza`)
     console.log(response)
     let data = await response.json()
     console.log(data)
+    useApiDataC(data)
 } 
+
+function useApiDataC(data) {
+    document.querySelector("#content1").innerHTML = `
+    <div class="card-search">
+    <img src="${data.hits[0].recipe.image}" class="img-card-search" alt="..."/>
+    <div class="card-body">
+        <h5 class="card-title-search">${data.hits[0].recipe.label}</h5>
+        <p class="card-text-search">${data.hits[0].recipe.source}</p>
+        <a href="${data.hits[0].recipe.url}" class="btn btn-primary">Ver mas</a>
+    </div>
+    </div>
+    `
+}
 
 
